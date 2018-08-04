880664c19a284ab99d2a02215bf93f0310ff7077
sjsullivan7 <sjsullivan7@gmail.com>
2018-08-03 23:58:53 -0400

    Added / endpoint so AWS ELBHealthChecker can check health

---
+++ src/main/java/com/github/ssullivan/resources/ELBHealthCheckerResource.java
@@ -0,0 +1,12 @@
+package com.github.ssullivan.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/")
+public class ELBHealthCheckerResource {
+  @GET
+  public String pong() {
+    return "pong";
+  }
+}
