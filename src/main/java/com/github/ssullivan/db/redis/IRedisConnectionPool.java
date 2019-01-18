2087f82a54558b3e2b3ad264c3bfaf4d80d4896f
sjsullivan7 <sjsullivan7@gmail.com>
2019-01-18 16:31:03 -0500

    init commit

--- src/main/java/com/github/ssullivan/db/redis/IRedisConnectionPool.java
+++ src/main/java/com/github/ssullivan/db/redis/IRedisConnectionPool.java
@@ -2,9 +2,10 @@ package com.github.ssullivan.db.redis;
 
 import com.google.inject.ImplementedBy;
 import io.lettuce.core.api.StatefulRedisConnection;
+import java.io.Closeable;
 
 @ImplementedBy(RedisConnectionPool.class)
-public interface IRedisConnectionPool {
+public interface IRedisConnectionPool  {
   StatefulRedisConnection<String, String> borrowConnection() throws Exception;
 
   boolean isClosed();
