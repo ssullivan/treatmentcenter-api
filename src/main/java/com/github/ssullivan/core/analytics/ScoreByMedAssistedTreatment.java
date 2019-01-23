3b43a107d26230e04aebd3292bb87e14b90c3eb4
sjsullivan7 <sjsullivan7@gmail.com>
2019-01-22 19:51:43 -0500

    treatment score

---
+++ src/main/java/com/github/ssullivan/core/analytics/ScoreByMedAssistedTreatment.java
@@ -0,0 +1,73 @@
+package com.github.ssullivan.core.analytics;
+
+import com.github.ssullivan.model.Facility;
+import java.util.Set;
+
+public class ScoreByMedAssistedTreatment implements IScoreFacility {
+  private static final String BMW = "BMW";
+  private static final String BU = "BU";
+  private static final String BUM = "BUM";
+  private static final String BUP = "BUP";
+  private static final String METH = "METH";
+  private static final String MM = "MM";
+  private static final String MMW = "MMW";
+  private static final String NMOA = "NMOA";
+  private static final String NXN = "NXN";
+  private static final String PAIN = "PAIN";
+  private static final String RPN = "RPN";
+  private static final String UBN = "UBN";
+  private static final String VTRL = "VTRL";
+  private static final String IMETH = "IMETH";
+  private static final String NALT = "NALT";
+  private static final String VIV = "VIV";
+
+  private static final String[] MED_CODES = new String[] {
+      BMW, BU, BUP, BUM, METH, MM, MMW, NXN, PAIN, RPN, UBN, VTRL, NALT, IMETH, VIV
+  };
+
+
+  private final Set<String> serviceCodes;
+  private final boolean useMeds;
+
+  public ScoreByMedAssistedTreatment(final Set<String> serviceCodes, final boolean useMeds) {
+    this.serviceCodes = serviceCodes;
+    this.useMeds = useMeds;
+  }
+
+  public ScoreByMedAssistedTreatment(final Set<String> serviceCodes) {
+    this(serviceCodes, Sets.anyMatch(serviceCodes,
+        MED_CODES));
+  }
+
+  @Override
+  public double score(final Facility facility) {
+    if (this.useMeds) {
+      if (noPref() && usesMeds(facility)) {
+        return 1.0;
+      }
+      if (Sets.anyMatch(serviceCodes, BUP) && facility.hasAnyOf(BMW, BU, BUM, PAIN, UBN)) {
+        return 1.0;
+      }
+      if (Sets.anyMatch(serviceCodes, IMETH) && facility.hasAnyOf(METH, MM, MMW, PAIN)) {
+        return 1.0;
+      }
+      if (Sets.anyMatch(serviceCodes, NALT) && facility.hasAnyOf(NXN, RPN, UBN)) {
+        return 1.0;
+      }
+      if (Sets.anyMatch(serviceCodes, VIV) && facility.hasAnyOf(VTRL)) {
+        return 1.0;
+      }
+    }
+    return 0;
+  }
+
+  private boolean usesMeds(final Facility facility) {
+    return facility.hasAnyOf(MED_CODES) && !facility.hasAnyOf(NMOA);
+  }
+
+  private boolean noPref() {
+    return serviceCodes == null || serviceCodes.isEmpty();
+  }
+
+
+}
--- src/main/java/com/github/ssullivan/core/analytics/Sets.java
+++ src/main/java/com/github/ssullivan/core/analytics/Sets.java
@@ -4,6 +4,7 @@ import java.util.Set;
 import java.util.stream.Stream;
 
 public final class Sets {
+
   public static boolean anyMatch(final Set<String> set, final String... items) {
     if (null == set || set.isEmpty() || items == null || items.length <= 0) return false;
     return set.stream().anyMatch(item -> Stream.of(items).anyMatch(it -> it.equalsIgnoreCase(item)));
