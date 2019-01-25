f4ba95300e3d79819ec0cbf7d8b6f5cbc08b95b6
Stephen Sullivan <sjsullivan7@gmail.com>
2019-01-24 19:41:46 -0500

    tests for med assist

---
+++ src/test/java/com/github/ssullivan/core/analytics/ScoreByMedsTest.java
@@ -0,0 +1,29 @@
+package com.github.ssullivan.core.analytics;
+
+import com.github.ssullivan.model.Facility;
+import com.google.common.collect.ImmutableSet;
+import org.hamcrest.Matchers;
+import org.hamcrest.junit.MatcherAssert;
+import org.junit.jupiter.api.Test;
+
+public class ScoreByMedsTest {
+  @Test
+  public void testScore() {
+    ScoreByMedAssistedTreatment s = new ScoreByMedAssistedTreatment(ImmutableSet.of("IMETH", "METH"), true);
+    Facility facility = new Facility();
+    facility.setServiceCodes(ImmutableSet.of("METH"));
+
+    double score = s.score(facility);
+    MatcherAssert.assertThat(score, Matchers.greaterThanOrEqualTo(1.0));
+  }
+
+  @Test
+  public void testScoreNoMeds() {
+    ScoreByMedAssistedTreatment s = new ScoreByMedAssistedTreatment(ImmutableSet.of("IMETH", "METH"), false);
+    Facility facility = new Facility();
+    facility.setServiceCodes(ImmutableSet.of("METH"));
+
+    double score = s.score(facility);
+    MatcherAssert.assertThat(score, Matchers.lessThan(1.0));
+  }
+}
