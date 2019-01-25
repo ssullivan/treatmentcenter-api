25ec1ca2fe547a424a688e53ce4555517e6cea8c
Stephen Sullivan <sjsullivan7@gmail.com>
2019-01-24 20:34:08 -0500

    Feature/tests (#17)
    * first commit
    
    * tests for med assist
    
    * Bump dropwizard for: Fix CVE-2018-10237 by upgrading Guava to 24.1.1
    
    * Add search with score endpoint
    
    * Bump apt
    
    * bump

--- src/main/java/com/github/ssullivan/resources/FacilitySearchResource.java
+++ src/main/java/com/github/ssullivan/resources/FacilitySearchResource.java
@@ -75,6 +75,7 @@ public class FacilitySearchResource {
     this.postalcodeService = postalcodeService;
   }
 
+
   @ApiOperation(value = "Find treatment facilities by their services and location. When multiple serviceCode, and matchAny sets are specified those results will be unified together",
       response = SearchResults.class)
   @ApiResponses(value = {
@@ -85,9 +86,9 @@ public class FacilitySearchResource {
           message = "An error occurred in the service while executing the search")
   })
   @GET
-  @Path("/v2/search")
+  @Path("/searchWithScore")
   @ManagedAsync
-  public void findFacilitiesByServiceCodesV2(final @Suspended AsyncResponse asyncResponse,
+  public void findFacilitiesByServiceCodesV2WithScore(final @Suspended AsyncResponse asyncResponse,
       @ApiParam(value = "a U.S. PostalCode. If a (lat,lon) is specified that will take precedence", allowMultiple = false)
       @QueryParam("postalCode") final String postalCode,
 
@@ -244,7 +245,7 @@ public class FacilitySearchResource {
       // protect against nefarious users sending too many sets in
       if (searchRequest.getConditions().size() > 15) {
         asyncResponse.resume(Response.status(400)
-        .entity(ImmutableMap.of("message", "too many search conditions."))
+            .entity(ImmutableMap.of("message", "too many search conditions."))
             .build());
         return;
       }
@@ -287,12 +288,151 @@ public class FacilitySearchResource {
       }
       this.facilityDao.find(searchRequest, Page.page(offset, size))
           .whenComplete((result, error) -> {
+            if (error != null) {
+              LOGGER.error("Failed to find facilities with service codes`", error);
+              asyncResponse.resume(Response.serverError().build());
+            }
+            else {
+              applyScores(searchRequest, scoreBuilder, result);
+              asyncResponse.resume(Response.ok(result).build());
+            }
+          });
+
+    } catch (Exception e) {
+      LOGGER.error("Failed to find facilities with service codes`", e);
+      asyncResponse.resume(Response.serverError().build());
+
+      if (e instanceof InterruptedException) {
+        LOGGER.error("Interrupted while handling request", e);
+        Thread.currentThread().interrupt();
+      }
+    }
+  }
+
+
+
+  @ApiOperation(value = "Find treatment facilities by their services and location. When multiple serviceCode, and matchAny sets are specified those results will be unified together",
+      response = SearchResults.class)
+  @ApiResponses(value = {
+      @ApiResponse(code = 200,
+          message = "Search was completed successfully", response = SearchResults.class),
+      @ApiResponse(code = 400, message = "Invalid query parameters"),
+      @ApiResponse(code = 500,
+          message = "An error occurred in the service while executing the search")
+  })
+  @GET
+  @Path("/v2/search")
+  @ManagedAsync
+  public void findFacilitiesByServiceCodesV2(final @Suspended AsyncResponse asyncResponse,
+      @ApiParam(value = "a U.S. PostalCode. If a (lat,lon) is specified that will take precedence", allowMultiple = false)
+      @QueryParam("postalCode") final String postalCode,
+
+
+      @ApiParam(value = "A comma separated list of service codes. service code prefixed with a single bang '!' will be negated", allowMultiple = true)
+      @QueryParam("serviceCode") final List<String> serviceCodes,
+
+      @ApiParam(value = "A comma separated list of service codes.", allowMultiple = true)
+      @QueryParam("matchAny") final List<String> matchAnyServiceCodes,
+
+      @ApiParam(value = "the latitude coordinate according to WGS84", allowableValues = "range[-90,90]")
+      @QueryParam("lat") final Double lat,
+
+      @ApiParam(value = "the longitude coordinate according to WGS84", allowableValues = "range[-180,180]")
+      @QueryParam("lon") final Double lon,
+
+      @ApiParam(value = "the radius distance") @DefaultValue("15")
+      @QueryParam("distance") final Double distance,
+
+      @ApiParam(value = "the unit of the radius distance. (meters, kilometers, feet, miles)", allowableValues = "m,km,ft,mi")
+      @Pattern(regexp = "m|km|ft|mi", message = "Invalid distance unit")
+      @DefaultValue("mi")
+      @QueryParam("distanceUnit") final String distanceUnit,
+
+      @ApiParam(value = "the number of results to skip", allowableValues = "range[0, 9999]")
+      @Min(0) @Max(9999) @DefaultValue("0") @QueryParam("offset") final int offset,
+
+      @ApiParam(value = "the number of results to return", allowableValues = "range[0, 9999]")
+      @Min(0) @Max(9999) @DefaultValue("10") @QueryParam("size") final int size,
+
+      @ApiParam(value="When multiple serviceCode, and matchAny sets are specified this controls how the final results are combined"
+          , allowableValues = "AND,OR", defaultValue = "AND")
+      @Pattern(regexp = "AND|OR", message = "Invalid boolean operator")
+      @DefaultValue("AND")
+      @QueryParam("operation")
+      final String op) {
+
+    try {
+
+      if (postalCode != null && postalCode.length() > 10) {
+        asyncResponse.resume(Response.status(400)
+            .entity(ImmutableMap.of("message", "Invalid postal code: Too Long"))
+            .build()
+        );
+      }
+
+      // Validate that the service codes are valid
+      final Optional<String> firstInvalidServiceCode = serviceCodes.stream()
+          .filter(code -> !RE_VALID_SAMSHA_SERVICE_CODE.matcher(code).matches())
+          .findFirst();
+
+      // If any of the service codes are invalid return a 400
+      if (firstInvalidServiceCode.isPresent()) {
+        asyncResponse.resume(Response.status(400)
+            .entity(ImmutableMap
+                .of("message", "Invalid service code: " + firstInvalidServiceCode.get())).build());
+        return;
+      }
+
+      final SearchRequest searchRequest = new SearchRequest();
+      final ServicesConditionFactory factory = new ServicesConditionFactory();
+      searchRequest.setFinalSetOperation(SetOperation.fromBooleanOp(op));
+      searchRequest.setServiceConditions(factory.fromRequestParams(serviceCodes, matchAnyServiceCodes));
+
+      // protect against nefarious users sending too many sets in
+      if (searchRequest.getConditions().size() > 15) {
+        asyncResponse.resume(Response.status(400)
+        .entity(ImmutableMap.of("message", "too many search conditions."))
+            .build());
+        return;
+      }
+
+      final Optional<ServicesCondition> invalidCondition = searchRequest.getConditions().stream()
+          .filter(it -> it.size() > 200)
+          .findAny();
+
+      if (invalidCondition.isPresent()) {
+        asyncResponse.resume(Response.status(400)
+            .entity(ImmutableMap.of("message", "too many search conditions."))
+            .build());
+        return;
+      }
+
+      if (lat != null && lon != null && !GeoPoint.isValidLatLong(lat, lon) && postalCode == null) {
+        asyncResponse.resume(
+            Response.status(400)
+                .entity(ImmutableMap.of("message", "Invalid lat, lon coordinate")));
+      } else if (lat != null && lon != null) {
+        searchRequest.setGeoRadiusCondition(new GeoRadiusCondition(GeoPoint.geoPoint(lat, lon), distance, distanceUnit));
+      } else if (postalCode != null) {
+        ImmutableList<GeoPoint> geoPoints = postalcodeService.fetchGeos(postalCode);
+        if (geoPoints == null || geoPoints.size() <= 0) {
+          LOGGER.error("Failed to find GeoPoints for PostCode", postalCode);
+          asyncResponse.resume(Response.status(400)
+              .entity(ImmutableMap.of("message", "Failed to Geo locate postal code"))
+              .build()
+          );
+        }
+        else {
+          searchRequest.setGeoRadiusCondition(new GeoRadiusCondition(geoPoints.get(0), distance, distanceUnit));
+        }
+      }
+      this.facilityDao.find(searchRequest, Page.page(offset, size))
+          .whenComplete((result, error) -> {
               if (error != null) {
                 LOGGER.error("Failed to find facilities with service codes`", error);
                 asyncResponse.resume(Response.serverError().build());
               }
               else {
-                applyScores(searchRequest, scoreBuilder, result);
                 asyncResponse.resume(Response.ok(result).build());
               }
           });
@@ -392,97 +532,7 @@ public class FacilitySearchResource {
       @Pattern(regexp = "AND|OR")
       @DefaultValue("AND")
       @QueryParam("operation")
-      final String op,
-
-      // Params for scoring
-
-      @ApiParam(value = "The users date of birth in YYYY-MM-DD format [used for scoring]", example = "1980-01-16", allowEmptyValue = true)
-      @Pattern(regexp = "\\d{4}-\\d{2}-\\d{2}", message = "Invalid date of birth")
-      @QueryParam("dob")
-      final String dateOfBirth,
-
-      @ApiParam(value = "The users gender [used for scoring]", allowableValues = "MALE,FEMALE", allowEmptyValue = true)
-      @Pattern(regexp = "MALE|FEMALE|male|female|Male|Female", message = "Invalid gender")
-      @QueryParam("gender")
-      final String gender,
-
-      // Params for heading support
-      @ApiParam(value = "true means hearing support is needed", allowableValues = TRUE_FALSE, allowEmptyValue = true)
-      @DefaultValue("false")
-      @QueryParam("hearingSupport")
-      final boolean hearingSupport,
-
-      @ApiParam(value = "How important it is that a facility provides hearing support services", allowableValues = VERY_SOMEWHAT_NOT, allowEmptyValue = true)
-      @DefaultValue("NOT")
-      @QueryParam("hearingSupportImp")
-      final Importance hearingSupportImportance,
-
-      // Params for Lang support
-      @ApiParam(value = "Indicates if english is your first language", allowableValues = TRUE_FALSE, allowEmptyValue = true)
-      @DefaultValue("true")
-      @QueryParam("englishFirst")
-      final boolean englishFirst,
-
-      @ApiParam(value = "How important it is that a facility provides language support services", allowableValues = VERY_SOMEWHAT_NOT, allowEmptyValue = true)
-      @DefaultValue("NOT")
-      @QueryParam("langSupportImp")
-      final Importance langSupportImp,
-
-      // Params for Med Assisted Treatment
-      @ApiParam(value = "Indicates if medication assisted treatment is needed/wanted", allowableValues = TRUE_FALSE, allowEmptyValue = true)
-      @DefaultValue("false")
-      @QueryParam("medassist")
-      final boolean useMeds,
-
-      // Params for Mental Health
-      @ApiParam(value = "Indicates if mental health plays a role in the addiction", allowableValues = TRUE_FALSE, allowEmptyValue = true)
-      @DefaultValue("false")
-      @QueryParam("mentalHealthRelated")
-      final boolean mentalHealthRelated,
-
-      // Params for Military Status,
-      @ApiParam(value = "Indicates if a facility with support for military is needed/wanted", allowableValues = TRUE_FALSE, allowEmptyValue = true)
-      @DefaultValue("false")
-      @QueryParam("militaryStatus")
-      final boolean militaryStatus,
-
-      @ApiParam(value = "Indicates how important military support is", allowableValues = VERY_SOMEWHAT_NOT, allowEmptyValue = true)
-      @DefaultValue("NOT")
-      @QueryParam("militaryImp")
-      final Importance militaryImp,
-
-      // Params for Service Setting
-
-      // Params for Smoking Cessation
-
-      @ApiParam(value = "Indicates if smoking cessation support is needed/wanted", allowableValues = TRUE_FALSE, allowEmptyValue = true)
-      @DefaultValue("false")
-      @QueryParam("smokingCessation")
-      final boolean smokingCessation,
-
-      // Params for Smoking Policy
-      @ApiParam(value = "Indicates if user is a smoker", allowableValues = TRUE_FALSE, allowEmptyValue = true)
-      @DefaultValue("false")
-      @QueryParam("smoker")
-      final boolean isSmoker,
-
-      // Params for Substance Detox Services
-      @ApiParam(value = "Indicates if user has started detox", allowableValues = TRUE_FALSE, allowEmptyValue = true)
-      @DefaultValue("false")
-      @QueryParam("detoxStarted")
-      final boolean detoxStarted,
-
-      // Params for Trauma Services
-      @ApiParam(value = "Indicates if trauma support is needed/wanted", allowableValues = TRUE_FALSE, allowEmptyValue = true)
-      @DefaultValue("false")
-      @QueryParam("traumaSupport")
-      final boolean traumaSupport,
-   
-      @ApiParam(value = "Indicates type of trauma support needed/wanted", allowableValues = TRAUMA_DOMESTIC_SEXUAL_NONE, allowEmptyValue = true, allowMultiple = true)
-      @DefaultValue("NONE")
-      @QueryParam("trauma")
-      final Set<TraumaTypes> traumaTypes
-      ) {
+      final String op) {
     try {
       if (postalCode != null && postalCode.length() > 10) {
         asyncResponse.resume(Response.status(400)
@@ -524,11 +574,6 @@ public class FacilitySearchResource {
         return;
       }
 
-      final Builder scoreBuilder = buildFromRequestParams(dateOfBirth, gender, hearingSupport,
-          hearingSupportImportance, englishFirst, langSupportImp, useMeds, mentalHealthRelated,
-          militaryStatus, militaryImp, smokingCessation, isSmoker, detoxStarted, traumaSupport,
-          traumaTypes);
-
       if (lat != null && lon != null && !GeoPoint.isValidLatLong(lat, lon) && postalCode == null) {
         asyncResponse.resume(
             Response.status(400)
@@ -556,13 +601,13 @@ public class FacilitySearchResource {
                 distanceUnit,
                 Page.page(offset, size));
 
-        asyncResponse.resume(applyScores(ImmutableSet.copyOf(mustServiceCodes), scoreBuilder, results));
+        asyncResponse.resume(results);
       }
       else {
         final SearchResults<Facility> results = this.facilityDao.findByServiceCodes(mustServiceCodes, mustNotServiceCodes,
             matchAny, Page.page(offset, size));
         asyncResponse
-            .resume(applyScores(ImmutableSet.copyOf(mustServiceCodes), scoreBuilder, results));
+            .resume(results);
       }
     } catch (IOException e) {
       LOGGER.error("Failed to find facilities with service codes`", e);
