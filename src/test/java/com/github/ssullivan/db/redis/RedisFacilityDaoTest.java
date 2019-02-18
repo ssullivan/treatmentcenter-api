2c303a80a79a9fbe55ca74fc05e7a34dd2cbfaa5
Stephen Sullivan <ssullivan@ctr.eitccorp.com>
2019-02-17 22:50:22 -0500

    Update tests for travis

--- src/test/java/com/github/ssullivan/db/redis/RedisFacilityDaoTest.java
+++ src/test/java/com/github/ssullivan/db/redis/RedisFacilityDaoTest.java
@@ -1,7 +1,11 @@
 package com.github.ssullivan.db.redis;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.ssullivan.RedisConfig;
+import com.github.ssullivan.db.ICategoryCodesDao;
+import com.github.ssullivan.db.IFacilityDao;
 import com.github.ssullivan.db.IFeedDao;
+import com.github.ssullivan.db.IServiceCodesDao;
 import com.github.ssullivan.db.redis.IRedisConnectionPool;
 import com.github.ssullivan.db.redis.RedisCategoryCodesDao;
 import com.github.ssullivan.db.redis.RedisFacilityDao;
@@ -23,11 +27,17 @@ import com.github.ssullivan.utils.ShortUuid;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Sets;
+import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import io.lettuce.core.KeyValue;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.sync.RedisCommands;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.TimeUnit;
+import org.assertj.core.util.Lists;
 import org.hamcrest.Matchers;
 import org.hamcrest.junit.MatcherAssert;
 import org.junit.jupiter.api.AfterAll;
@@ -37,49 +47,54 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.mockito.Mockito;
 
 @TestInstance(Lifecycle.PER_CLASS)
 public class RedisFacilityDaoTest {
 
   private static final String FeedId = ShortUuid.randomShortUuid();
   private static final String FirstId = ShortUuid.randomShortUuid();
-  private static final String SecondId = ShortUuid.randomShortUuid();
-  private static final String ThirdId = ShortUuid.randomShortUuid();
 
-  private RedisFacilityDao _dao;
-  private RedisCategoryCodesDao _categoryCodesDao;
-  private RedisServiceCodeDao _serviceCodesDao;
-  private IRedisConnectionPool _redisConnectionPool;
-
-  @BeforeAll
-  private void setup() throws IOException {
-    final RedisConfig redisConfig = new RedisConfig("127.0.0.1", 6379, 1);
-    redisConfig.setTimeout(50);
-
-    final Injector injector = Guice
-        .createInjector(new RedisClientModule(redisConfig));
-    _dao = injector.getInstance(RedisFacilityDao.class);
-    _categoryCodesDao = injector.getInstance(RedisCategoryCodesDao.class);
-    _serviceCodesDao = injector.getInstance(RedisServiceCodeDao.class);
-    _redisConnectionPool = injector.getInstance(IRedisConnectionPool.class);
-
-    IFeedDao feedDao = injector.getInstance(IFeedDao.class);
-
-    final String nextFeedId = feedDao.nextFeedId().get();
-
-    feedDao.setCurrentFeedId(nextFeedId);
-    feedDao.setSearchFeedId(nextFeedId);
-  }
+  private static final com.fasterxml.jackson.databind.ObjectMapper ObjectMapper = new ObjectMapper();
+  private IRedisConnectionPool redisPool;
+  private StatefulRedisConnection<String, String> redisConnection;
+  private RedisCommands<String, String> redisCommand;
+  private Injector injector;
+  private ICategoryCodesDao categoryCodesDao;
+  private IFeedDao feedDao;
+  private IServiceCodesDao serviceCodesDao;
+  private IFacilityDao dao;
 
 
-  @AfterEach
-  private void flushDb() throws Exception {
-    _redisConnectionPool.borrowConnection().sync().flushdb();
-  }
 
-  @AfterAll
-  private void teardown() throws Exception {
-    _redisConnectionPool.close();
+  @BeforeEach()
+  public void initEach() throws Exception {
+    redisPool = Mockito.mock(IRedisConnectionPool.class);
+    redisConnection = (StatefulRedisConnection<String, String>) Mockito
+        .mock(StatefulRedisConnection.class);
+    redisCommand = (RedisCommands<String, String>) Mockito.mock(RedisCommands.class);
+
+    Mockito.when(redisConnection.sync()).thenReturn(redisCommand);
+    Mockito.when(redisPool.borrowConnection()).thenReturn(redisConnection);
+    Mockito.when(redisPool.borrowConnection(Mockito.anyLong())).thenReturn(redisConnection);
+
+    categoryCodesDao = Mockito.mock(ICategoryCodesDao.class);
+    serviceCodesDao = Mockito.mock(IServiceCodesDao.class);
+    feedDao = Mockito.mock(IFeedDao.class);
+
+    injector = Guice.createInjector(new AbstractModule() {
+      @Override
+      protected void configure() {
+        bind(IRedisConnectionPool.class).toInstance(redisPool);
+        bind(ObjectMapper.class).toInstance(ObjectMapper);
+
+        bind(ICategoryCodesDao.class).toInstance(categoryCodesDao);
+        bind(IServiceCodesDao.class).toInstance(serviceCodesDao);
+        bind(IFeedDao.class).toInstance(feedDao);
+      }
+    });
+
+    dao = injector.getInstance(IFacilityDao.class);
   }
 
 
@@ -96,9 +111,14 @@ public class RedisFacilityDaoTest {
     original.setWebsite("http://www.test.com");
     original.setZip("10001");
 
-    _dao.addFacility(FeedId, original);
+    final String json = ObjectMapper.writeValueAsString(original);
 
-    final Facility fromDb = _dao.getFacility(original.getId());
+    Mockito.when(redisCommand.hmget(Mockito.anyString(), Mockito.eq("_source")))
+        .thenReturn(Lists.newArrayList(KeyValue.just("_source", json)));
+
+    dao.addFacility(FeedId, original);
+
+    final Facility fromDb = dao.getFacility(original.getId());
     MatcherAssert.assertThat(fromDb, Matchers.notNullValue());
     MatcherAssert.assertThat(fromDb.getId(), Matchers.equalTo(original.getId()));
     MatcherAssert.assertThat(fromDb.getCategoryCodes(), Matchers.containsInAnyOrder("TEST"));
