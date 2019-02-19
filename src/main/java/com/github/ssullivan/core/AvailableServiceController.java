5a73fc0ccc18c02e31c54007a4b12bd7e6fb41b5
sjsullivan7 <sjsullivan7@gmail.com>
2019-02-18 23:07:21 -0500

    Service to refresh cats from db every X minutes

--- src/main/java/com/github/ssullivan/core/AvailableServiceController.java
+++ src/main/java/com/github/ssullivan/core/AvailableServiceController.java
@@ -9,14 +9,20 @@ import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Sets;
+import com.google.common.util.concurrent.AbstractScheduledService;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.ListeningExecutorService;
 import com.google.common.util.concurrent.MoreExecutors;
+import com.google.common.util.concurrent.ServiceManager;
+import com.google.common.util.concurrent.ServiceManager.Listener;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import io.dropwizard.lifecycle.Managed;
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
@@ -33,6 +39,7 @@ public class AvailableServiceController implements IAvailableServiceController, 
 
   private final ICategoryCodesDao categoryCodesDao;
   private final LoadingCache<String, Category> cache;
+  private ServiceManager serviceManager;
 
   @Inject
   public AvailableServiceController(final ICategoryCodesDao categoryCodesDao) {
@@ -41,8 +48,25 @@ public class AvailableServiceController implements IAvailableServiceController, 
         .concurrencyLevel(8)
         .maximumSize(256)
         .refreshAfterWrite(30, TimeUnit.MINUTES)
-        .expireAfterAccess(1, TimeUnit.DAYS)
+        .expireAfterWrite(3, TimeUnit.DAYS)
         .build(new CategoryCacheLoader(pool, categoryCodesDao));
+    this.serviceManager = new ServiceManager(Sets.newHashSet(new RefreshCache(this.cache, this.categoryCodesDao)));
+    this.serviceManager.addListener(new Listener() {
+      @Override
+      public void healthy() {
+        LOGGER.info("Refresh Cache Service is healthy");
+      }
+
+      @Override
+      public void stopped() {
+        LOGGER.warn("Refresh Cache Service has died");
+      }
+
+      @Override
+      public void failure(final com.google.common.util.concurrent.Service service) {
+        LOGGER.error("Refresh cache Service has failed", service.failureCause());
+      }
+    });
   }
 
   @Override
@@ -53,28 +77,35 @@ public class AvailableServiceController implements IAvailableServiceController, 
 
   @Override
   public Facility apply(final Facility facility) {
+    if (facility == null) return null;
+
     final ImmutableMap<String, Category> cats = this.cache.getAllPresent(facility.getCategoryCodes());
 
+    if (cats == null || cats.isEmpty()) {
+      LOGGER.warn("Cache Miss for facility {} categories: {}", facility.getId(), facility.getCategoryCodes());
+      facility.setAvailableServices(new AvailableServices());
+    }
+    else {
+      // For the available services object we only want the category objects included in it
+      // to contains services that the facility actually has
+      final Set<Category> facilityCats = cats.values().stream().map(category -> {
+        // This contains only the service objects that the facility actually has
+        final Set<Service> facilityServices =
+            category.getServices().stream().filter(service -> facility.hasAnyOf(service.getCode()))
+                .collect(
+                    Collectors.toSet());
 
-    // For the available services object we only want the category objects included in it
-    // to contains services that the facility actually has
-    final Set<Category> facilityCats = cats.values().stream().map(category -> {
-      // This contains only the service objects that the facility actually has
-      final Set<Service> facilityServices =
-          category.getServices().stream().filter(service -> facility.hasAnyOf(service.getCode())).collect(
-          Collectors.toSet());
+        // Create a new category that only has the services the facility has
+        return new Category(category.getCode(), category.getName(), facilityServices);
+      }).collect(Collectors.toSet());
 
-      // Create a new category that only has the services the facility has
-      return new Category(category.getCode(), category.getName(), facilityServices);
-    }).collect(Collectors.toSet());
-
-
-    facility.setAvailableServices(new AvailableServices(facilityCats));
+      facility.setAvailableServices(new AvailableServices(facilityCats));
+    }
 
     return facility;
   }
 
-  public void refreshAll() {
+  private void refreshAll() {
     try {
       LOGGER.info("Refreshing category list from db");
       final List<Category> categoryList = this.categoryCodesDao.listCategories();
@@ -87,6 +118,7 @@ public class AvailableServiceController implements IAvailableServiceController, 
 
   @Override
   public void start() throws Exception {
+    this.serviceManager.startAsync();
     refreshAll();
   }
 
@@ -96,7 +128,40 @@ public class AvailableServiceController implements IAvailableServiceController, 
   }
 
   /**
-   * CacheLoader that can asycn refresh the cache
+   * Periodically refresh the cache from the database.
+   */
+  private static final class RefreshCache extends AbstractScheduledService implements
+      com.google.common.util.concurrent.Service {
+    private LoadingCache<String, Category> cache;
+    private ICategoryCodesDao dao;
+
+    private RefreshCache(final LoadingCache<String, Category> cache, final ICategoryCodesDao dao) {
+      this.cache = cache;
+      this.dao = dao;
+    }
+
+    @Override
+    protected void runOneIteration() throws Exception {
+      try {
+        LOGGER.info("Refreshing category list from db");
+        final List<Category> categoryList = this.dao.listCategories();
+        categoryList.forEach(cat -> this.cache.put(cat.getCode(), cat));
+        LOGGER.info("Loaded {} cats", categoryList.size());
+      } catch (IOException e) {
+        LOGGER.error("Failed to list categories!", e);
+      }
+    }
+
+    @Override
+    protected Scheduler scheduler() {
+      return Scheduler.newFixedRateSchedule(TimeUnit.MINUTES.toMillis(1),
+          TimeUnit.MINUTES.toMillis(10),
+          TimeUnit.MILLISECONDS);
+    }
+  }
+
+  /**
+   * CacheLoader that can async refresh the cache
    */
   private static final class CategoryCacheLoader extends CacheLoader<String, Category> {
     private final ListeningExecutorService executorService;
