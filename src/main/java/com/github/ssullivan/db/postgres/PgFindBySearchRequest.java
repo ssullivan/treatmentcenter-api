d700816150318ae3542ee7ddb508e0aebe35bcd0
sjsullivan7 <sjsullivan7@gmail.com>
2019-03-04 22:55:28 -0500

    needed to add sphere setting also

--- src/main/java/com/github/ssullivan/db/postgres/PgFindBySearchRequest.java
+++ src/main/java/com/github/ssullivan/db/postgres/PgFindBySearchRequest.java
@@ -54,7 +54,7 @@ public class PgFindBySearchRequest implements IFindBySearchRequest {
   }
 
   public static Condition _ST_DWithin(final GeoPoint geoPoint, final double radius, final GeoUnit unit) {
-    return DSL.condition("ST_DWithin(location.geog, 'SRID=4326;POINT(" + geoPoint.lon() + " " + geoPoint.lat() + ")', ?)", unit.convertTo(GeoUnit.METER, radius));
+    return DSL.condition("ST_DWithin(location.geog, 'SRID=4326;POINT(" + geoPoint.lon() + " " + geoPoint.lat() + ")'," +  unit.convertTo(GeoUnit.METER, radius) + ",true)");
   }
 
   @Override
