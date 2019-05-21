4ad9eeada0550bffe0a311def73f515366959e7d
Stephen Sullivan <ssullivan@ctr.eitccorp.com>
2019-05-20 23:10:46 -0400

    worksheet db code

--- src/main/java/com/github/ssullivan/db/postgres/PgJsonBUtils.java
+++ src/main/java/com/github/ssullivan/db/postgres/PgJsonBUtils.java
@@ -1,11 +1,16 @@
 package com.github.ssullivan.db.postgres;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.common.collect.Maps;
 import org.jooq.Condition;
 import org.jooq.DataType;
 import org.jooq.Field;
 import org.jooq.impl.DSL;
 import org.jooq.impl.SQLDataType;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public final class PgJsonBUtils {
     private static final PgJSONBJacksonBinding PG_JSONB_JACKSON_BINDING = new PgJSONBJacksonBinding();
@@ -13,6 +18,24 @@ public final class PgJsonBUtils {
 
     public static Condition jsonContains(final Field<JsonNode> field, final JsonNode value) {
         return DSL.condition("{0} @> {1}::jsonb", field, DSL.val(value, field));
+    }
+
+    public static <T> Condition jsonContains(final Field<JsonNode> field, final Map<String, Object> keyValues) {
+        return jsonContains(field, PG_JSONB_JACKSON_BINDING.converter().from(keyValues));
+    }
+
+    public static <T> Condition jsonContains(final Field<JsonNode> field, Map.Entry<String, T> entry) {
+        final Map<String, T> keyValue = new LinkedHashMap<>(1);
+        keyValue.put(entry.getKey(), entry.getValue());
+
+        return jsonContains(field, PG_JSONB_JACKSON_BINDING.converter().from(keyValue));
+    }
+
+    public static <T> Condition jsonContains(final Field<JsonNode> field, final String key, T value) {
+        final Map<String, T> keyValue = new LinkedHashMap<>(1);
+        keyValue.put(key, value);
+
+        return jsonContains(field, PG_JSONB_JACKSON_BINDING.converter().from(keyValue));
     }
 
     public static Condition jsonExists(final Field<JsonNode> field, final String fieldName) {
--- src/main/java/com/github/ssullivan/db/postgres/WorksheetDao.java
+++ src/main/java/com/github/ssullivan/db/postgres/WorksheetDao.java
@@ -7,7 +7,8 @@ import com.github.ssullivan.db.psql.Tables;
 import com.github.ssullivan.model.Page;
 import com.github.ssullivan.model.SearchResults;
 import com.github.ssullivan.model.collections.Tuple2;
-import org.jooq.DSLContext;
+import com.google.common.collect.Lists;
+import org.jooq.*;
 import org.jooq.impl.DSL;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -30,9 +31,9 @@ public class WorksheetDao implements IWorksheetDao {
 
     @Override
     public long getLatestVersion(final String spreadsheetId, final int worksheetId) {
-        return dsl.select(DSL.max(Tables.SHEET_ROWS.VERSION))
-                .where(Tables.SHEET_ROWS.SPREADSHEET_ID.eq(spreadsheetId)
-                        .and(Tables.SHEET_ROWS.SHEET_ID.eq(worksheetId)))
+        return dsl.select(DSL.max(Tables.WORKSHEET.VERSION))
+                .where(Tables.WORKSHEET.SPREADSHEET_ID.eq(spreadsheetId)
+                        .and(Tables.WORKSHEET.ID.eq(worksheetId)))
                 .limit(1)
                 .fetchOne()
                 .value1();
@@ -40,7 +41,15 @@ public class WorksheetDao implements IWorksheetDao {
 
     @Override
     public List<JsonNode> listWorksheets(Page page) {
-        return null;
+        return dsl.selectDistinct(Tables.WORKSHEET.SPREADSHEET_ID, Tables.WORKSHEET.ID, Tables.WORKSHEET.NAME)
+                .orderBy(Tables.WORKSHEET.ID)
+                .offset(page.offset())
+                .limit(page.size())
+                .fetch()
+                .map(record -> objectMapper.createObjectNode()
+                        .put(Tables.WORKSHEET.SPREADSHEET_ID.getName(), record.value1())
+                        .put(Tables.WORKSHEET.ID.getName(), record.value2())
+                        .put(Tables.WORKSHEET.NAME.getName(), record.value3()));
     }
 
     @Override
@@ -50,12 +59,54 @@ public class WorksheetDao implements IWorksheetDao {
 
     @Override
     public List<String> getColumnHeaders(String spreadsheetId, int worksheetId) {
-        return null;
+        return dsl.transactionResult((TransactionalCallable<List<String>>) configuration -> {
+            final DSLContext innerDsl = DSL.using(configuration);
+
+            long maxVersion = innerDsl.select(DSL.max(Tables.WORKSHEET.ID))
+                    .from(Tables.WORKSHEET)
+                    .where(Tables.WORKSHEET.SPREADSHEET_ID.eq(spreadsheetId)
+                            .and(Tables.WORKSHEET.ID.eq(worksheetId)))
+                    .limit(1)
+                    .fetchOne().value1();
+
+           final String[] headers = innerDsl.select(Tables.WORKSHEET.COLUMN_HEADERS)
+                    .from(Tables.WORKSHEET)
+                    .where(Tables.WORKSHEET.VERSION.eq(maxVersion)
+                        .and(Tables.WORKSHEET.SPREADSHEET_ID.eq(spreadsheetId)
+                                .and(Tables.WORKSHEET.ID.eq(worksheetId))))
+                    .orderBy(Tables.WORKSHEET.ID)
+                    .limit(1)
+                    .fetchOne()
+                    .value1();
+
+           return Lists.newArrayList(headers);
+        });
     }
 
     @Override
     public List<String> getColumnHeaders(String spreadsheetId, String worksheetName) {
-        return null;
+        return dsl.transactionResult((TransactionalCallable<List<String>>) configuration -> {
+            final DSLContext innerDsl = DSL.using(configuration);
+
+            long maxVersion = innerDsl.select(DSL.max(Tables.WORKSHEET.ID))
+                    .from(Tables.WORKSHEET)
+                    .where(Tables.WORKSHEET.SPREADSHEET_ID.eq(spreadsheetId)
+                            .and(Tables.WORKSHEET.NAME.eq(worksheetName)))
+                    .limit(1)
+                    .fetchOne().value1();
+
+            final String[] headers = innerDsl.select(Tables.WORKSHEET.COLUMN_HEADERS)
+                    .from(Tables.WORKSHEET)
+                    .where(Tables.WORKSHEET.VERSION.eq(maxVersion)
+                            .and(Tables.WORKSHEET.SPREADSHEET_ID.eq(spreadsheetId)
+                                    .and(Tables.WORKSHEET.NAME.eq(worksheetName))))
+                    .orderBy(Tables.WORKSHEET.ID)
+                    .limit(1)
+                    .fetchOne()
+                    .value1();
+
+            return Lists.newArrayList(headers);
+        });
     }
 
     @Override
@@ -70,11 +121,76 @@ public class WorksheetDao implements IWorksheetDao {
 
     @Override
     public SearchResults<JsonNode> findRows(String spreadsheetId, int worksheetId, Map<String, Object> params, Page page) {
-        return null;
+        return dsl.transactionResult(configuration -> {
+            final DSLContext innerDsl = DSL.using(configuration);
+
+            long maxVersion = innerDsl.select(DSL.max(Tables.WORKSHEET.ID))
+                    .from(Tables.WORKSHEET)
+                    .where(Tables.WORKSHEET.SPREADSHEET_ID.eq(spreadsheetId)
+                            .and(Tables.WORKSHEET.ID.eq(worksheetId)))
+                    .limit(1)
+                    .fetchOne().value1();
+
+            Condition condition = Tables.WORKSHEET.SPREADSHEET_ID.eq(spreadsheetId)
+                    .and(Tables.WORKSHEET.ID.eq(worksheetId)).and(Tables.WORKSHEET.VERSION.eq(maxVersion));
+
+            if (null != params && !params.isEmpty()) {
+                condition = condition.and(PgJsonBUtils.jsonContains(Tables.WORKSHEET.ROW_JSONB, params));
+            }
+
+            int totalResults = innerDsl.select(DSL.count(Tables.WORKSHEET.PK))
+                    .from(Tables.WORKSHEET)
+                    .where(condition)
+                    .fetchOne()
+                    .value1();
+
+            List<JsonNode> results = innerDsl.select(Tables.WORKSHEET.ROW_JSONB)
+                    .from(Tables.WORKSHEET)
+                    .where(condition)
+                    .orderBy(Tables.WORKSHEET.ROW_INDEX)
+                    .offset(page.offset())
+                    .limit(page.size())
+                    .fetch(Tables.WORKSHEET.ROW_JSONB);
+
+            return SearchResults.searchResults(totalResults, results);
+        });
     }
 
     @Override
     public SearchResults<JsonNode> findRows(String spreadsheetId, String worksheetName, Map<String, Object> params, Page page) {
-        return null;
+        return dsl.transactionResult(configuration -> {
+            final DSLContext innerDsl = DSL.using(configuration);
+
+            long maxVersion = innerDsl.select(DSL.max(Tables.WORKSHEET.ID))
+                    .from(Tables.WORKSHEET)
+                    .where(Tables.WORKSHEET.SPREADSHEET_ID.eq(spreadsheetId)
+                            .and(Tables.WORKSHEET.NAME.eq(worksheetName)))
+                    .limit(1)
+                    .fetchOne().value1();
+
+            Condition condition = Tables.WORKSHEET.SPREADSHEET_ID.eq(spreadsheetId)
+                    .and(Tables.WORKSHEET.NAME.eq(worksheetName)).and(Tables.WORKSHEET.VERSION.eq(maxVersion));
+
+            if (null != params && !params.isEmpty()) {
+                condition = condition.and(PgJsonBUtils.jsonContains(Tables.WORKSHEET.ROW_JSONB, params));
+            }
+
+            int totalResults = innerDsl.select(DSL.count(Tables.WORKSHEET.PK))
+                    .from(Tables.WORKSHEET)
+                    .where(condition)
+                    .fetchOne()
+                    .value1();
+
+            List<JsonNode> results = innerDsl.select(Tables.WORKSHEET.ROW_JSONB)
+                    .from(Tables.WORKSHEET)
+                    .where(condition)
+                    .orderBy(Tables.WORKSHEET.ROW_INDEX)
+                    .offset(page.offset())
+                    .limit(page.size())
+                    .fetch(Tables.WORKSHEET.ROW_JSONB);
+
+            return SearchResults.searchResults(totalResults, results);
+        });
     }
+
 }
